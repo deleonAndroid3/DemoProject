51c05667c7af8c79b79aef6b7d57cc410681fb78
deleonAndroid3 <alolzz123@gmail.com>
2020-09-15 13:18:18 +0800

    change in test

--- __tests__/myTest.test.js
+++ __tests__/myTest.test.js
@@ -33,10 +33,16 @@ const driver = wd.promiseRemote('https://johncarlodeleon1:EPnWvRvF8sxcKEC9xpat@h
 
 driver.init(capabilities)
 .then(function(){
-  return driver.waitForElementByAccessibilityId('nameInput', asserters.isDisplayed && asserters.isEnabled, 30000).sendKeys('Carlo');
+  return driver.waitForElementByAccessibilityId('nameInput', asserters.isDisplayed && asserters.isEnabled, 30000);
+})
+.then(function (nameInput) {
+  return nameInput.sendKeys('Carlo');
 })
 .then(function(){
-  return driver.waitForElementByAccessibilityId('saveName', asserters.isDisplayed && asserters.isEnabled, 30000).click();
+  return driver.waitForElementByAccessibilityId('saveName', asserters.isDisplayed && asserters.isEnabled, 30000);
+})
+.then(function (saveName) {
+  saveName.click();
 })
 .fin(function(){return driver.quit(); })
 .done();
