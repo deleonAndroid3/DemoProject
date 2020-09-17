03baeacf4f38ab56735b909dd4217df4f4095a44
deleonAndroid3 <alolzz123@gmail.com>
2020-09-17 12:29:20 +0800

    yarn test

---
+++ __tests__/local.test.js
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+import wd from 'wd';
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+const PORT = 4723;
+const config = {
+  platformName: 'Android',
+  deviceName: 'Android Emulator',
+  app: 'C:/Users/Carlo/Documents/1.Projects/React Native/DemoProject/android/app/build/outputs/apk/debug/app-debug.apk',
+};
+const driver = wd.promiseChainRemote('localhost', PORT);
+
+beforeAll(async () => {
+    await driver.init(config);
+    await driver.sleep(3000);
+  }); // Sometime for the app to load
+
+test('Correct Input', async () => {
+    let element = await driver.elementByAccessibilityId('nameInput');
+    await element.type('Carlo');
+    await driver.elementByAccessibilityId('saveName').click();
+  });
+
+test('Wrong Input', async () => {
+    let element = await driver.elementByAccessibilityId('nameInput');
+    await element.type('Carlo123');
+    await driver.elementByAccessibilityId('saveName').click();
+  });
+
+test('Empty Input', async () => {
+    let element = await driver.elementByAccessibilityId('nameInput');
+    await element.type('');
+    await driver.elementByAccessibilityId('saveName').click();
+  });
--- __tests__/myTest.test.js
+++ __tests__/myTest.test.js
@@ -1,8 +1,7 @@
 /* eslint-disable prettier/prettier */
 
 var wd = require('wd');
-var assert = require('assert');
-var asserters = wd.asserters;
+
 
 var app = process.env.BROWSERSTACK_APP_ID;
 
@@ -17,38 +16,15 @@ var capabilities = {
   'name': 'Unit Test',
 };
 
-const driver = wd.promiseRemote('https://hub-cloud.browserstack.com/wd/hub');
+const driver = wd.promiseRemote('https://johncarlodeleon1:EPnWvRvF8sxcKEC9xpat@hub-cloud.browserstack.com/wd/hub');
 
 driver.init(capabilities)
 .then(async function(){
-  return driver.elementByAccessibilityId('nameInput').sendKeys('Carlo');
+  return driver.elementByAccessibilityId('nameInput');
 })
 .then(async function(){
-  return driver.elementByAccessibilityId('saveName').click();
+  return driver.elementByAccessibilityId('saveName');
 })
 // .fin(function(){return driver.quit(); })
 .done();
-
-// beforeAll(async () => {
-//     await driver.init(capabilities);
-//     await driver.sleep(3000);
-//   }); // Sometime for the app to load
-
-// test('Correct Input', async () => {
-//     let element = await driver.elementByAccessibilityId('nameInput');
-//     await element.type('Carlo');
-//     await driver.elementByAccessibilityId('saveName').click();
-//   });
-
-// test('Wrong Input', async () => {
-//     let element = await driver.elementByAccessibilityId('nameInput');
-//     await element.type('Carlo123');
-//     await driver.elementByAccessibilityId('saveName').click();
-//   });
-
-// test('Empty Input', async () => {
-//     let element = await driver.elementByAccessibilityId('nameInput');
-//     await element.type('');
-//     await driver.elementByAccessibilityId('saveName').click();
-//   });
 
