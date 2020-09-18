d3346f65efb2a585726f7c7297875e49e69881a2
deleonAndroid3 <alolzz123@gmail.com>
2020-09-18 11:45:03 +0800

    change context

--- android/singletest.js
+++ android/singletest.js
@@ -8,16 +8,20 @@ var app = process.env.BROWSERSTACK_APP_ID;
 var desiredCaps = {
   'browserstack.user': 'johncarlodeleon1',
   'browserstack.key': 'EPnWvRvF8sxcKEC9xpat',
+  'automationName': 'Appium',
   build: 'Node Android',
   name: 'single_test',
   device: 'Google Pixel 3',
   app: app,
   'browserstack.debug': true,
+//   'browserstack.local': true,
 };
 var driver = wd.promiseRemote('http://hub-cloud.browserstack.com/wd/hub');
+let contexts =  driver.contexts();
 
 driver
   .init(desiredCaps)
+  .contexts(contexts[1])
   .then(function () {
     return driver.waitForElementByAccessibilityId(
       'saveName',
